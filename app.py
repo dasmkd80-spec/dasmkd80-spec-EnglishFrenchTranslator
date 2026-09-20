9bc9ac60bb6d57d5b745ac22432dbca6b2cc3935
dasmkd80-spec <dasmkd80@gmail.com>
2026-09-20 21:41:26 +0530

    Use Hugging Face inference for translation

--- app.py
+++ app.py
@@ -1,37 +1,23 @@
 from flask import Flask, render_template, request
-from transformers import MarianTokenizer, MarianMTModel
+from huggingface_hub import InferenceClient
+import os
 
 app = Flask(__name__)
 
-model_name = "Helsinki-NLP/opus-mt-en-fr"
+# Hugging Face Inference Client
+client = InferenceClient(
+    provider="hf-inference",
+    api_key=os.environ["HF_TOKEN"]
+)
 
-# Lazy loading
-tokenizer = None
-model = None
-
-def load_model():
-    global tokenizer, model
-    if tokenizer is None or model is None:
-        tokenizer = MarianTokenizer.from_pretrained(model_name)
-        model = MarianMTModel.from_pretrained(model_name)
 
 def translate_to_french(english_text):
-    load_model()   # Model first request appudu matrame load avutundi
-
-    encoded = tokenizer(
+    result = client.translation(
         english_text,
-        return_tensors="pt",
-        padding=True,
-        truncation=True
+        model="google-t5/t5-small"
     )
 
-    translated_tokens = model.generate(**encoded)
-    french_text = tokenizer.decode(
-        translated_tokens[0],
-        skip_special_tokens=True
-    )
-
-    return french_text
+    return result.translation_text
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -40,9 +26,14 @@ def home():
 
     if request.method == "POST":
         english_text = request.form["english"]
-        translation = translate_to_french(english_text)
 
-    return render_template("index.html", translation=translation)
+        if english_text.strip():
+            translation = translate_to_french(english_text)
+
+    return render_template(
+        "index.html",
+        translation=translation
+    )
 
 
 if __name__ == "__main__":
